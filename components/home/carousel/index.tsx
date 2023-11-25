7f8d349822e2e744098af29ceed9257481aa632d
Yan Kaiky <yankaikys@gmail.com>
2023-11-25 17:46:49 -0300

    feat(HomeCarousel): add carousel images

--- components/home/carousel/index.tsx
+++ components/home/carousel/index.tsx
@@ -1,23 +1,74 @@
-import { View } from 'react-native';
-import React from 'react';
+import { Animated, FlatList, Image, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
 import styles from './styles';
 import { COLORS } from '../../../constants';
 
 export const HomeCarousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const scrollX = useRef(new Animated.Value(0)).current;
+
+    let flatList = useRef<FlatList<string> | null>();
+
     const slides = [
         'https://e-commerce-forniture.s3.sa-east-1.amazonaws.com/closet.png',
         'https://e-commerce-forniture.s3.sa-east-1.amazonaws.com/sofa.png',
         'https://e-commerce-forniture.s3.sa-east-1.amazonaws.com/tv-panel.png',
-    ]
+        'https://e-commerce-forniture.s3.sa-east-1.amazonaws.com/kitchen.jpg',
+    ];
+
+    const onViewRef = useRef(({ changed }: { changed: any }) => {
+        if (changed[0].isViewable) {
+            setCurrentIndex(changed[0].index);
+        }
+    })
+
+    const scrollToIndex = (index: number) => {
+        flatList.current?.scrollToIndex({ animated: true, index });
+    };
 
     return (
-        <View style={styles.container}>
-            {/* <SliderBox
-                images={slides}
-                dotColor={COLORS.primary}
-                inactiveDotColor={COLORS.secondary}
-                ImageComponentStyle={styles.imageComponentStyle}
-            /> */}
+        <View>
+            <View style={styles.container}>
+                <FlatList
+                    data={slides}
+                    horizontal
+                    pagingEnabled
+                    bounces={false}
+                    keyExtractor={(item) => item}
+                    showsHorizontalScrollIndicator={false}
+                    ref={(ref) => flatList.current = ref}
+                    renderItem={({ item }) => {
+                        return (
+                            <TouchableOpacity onPress={() => console.log(item)} activeOpacity={1}>
+                                <Image source={{ uri: item }} style={styles.image} />
+                            </TouchableOpacity>
+                        )
+                    }}
+                    onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+                        useNativeDriver: false,
+                    })}
+                    style={styles.carousel}
+                    onViewableItemsChanged={onViewRef.current}
+                />
+
+                <View style={styles.dots}>
+                    {
+                        slides.map((_: any, i: number) => (
+                            <TouchableOpacity
+                                key={i}
+                                style={[
+                                    styles.circle,
+                                    {
+                                        backgroundColor: i === currentIndex ? COLORS.primary : COLORS.gray
+                                    }
+                                ]}
+                                onPress={() => scrollToIndex(i)}
+                            />
+                        ))
+                    }
+                </View>
+            </View>
         </View>
     );
 }
--- components/home/carousel/styles.ts
+++ components/home/carousel/styles.ts
@@ -1,14 +1,38 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+
+import { COLORS, SIZES } from '../../../constants';
+
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
+        borderRadius: SIZES.small,
     },
-    imageComponentStyle: {
-        borderRadius: 15,
-        width: '95%',
-        marginTop: 15,
+    carousel: {
+        maxHeight: width / 2,
+    },
+    image: {
+        width: width - 20,
+        height: width / 2,
+        justifyContent: 'center',
+        resizeMode: 'cover',
+        borderRadius: SIZES.small,
+        marginHorizontal: 10,
+    },
+    dots: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 20,
+    },
+    circle: {
+        width: 10,
+        height: 10,
+        backgroundColor: COLORS.primary,
+        borderRadius: 50,
+        marginHorizontal: 5,
     }
 });
 
